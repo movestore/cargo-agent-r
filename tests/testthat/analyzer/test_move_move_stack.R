caa9950b0a5afa61b780fdf68f3188de270d4e3d
Clemens Hahn <clemens@couchbits.com>
2023-01-10 12:53:27 +0100

    add more tests

--- tests/testthat/analyzer/test_move_move_stack.R
+++ tests/testthat/analyzer/test_move_move_stack.R
@@ -1,9 +1,19 @@
-source("../../src/analyzer/move_move_stack.R", chdir = TRUE)
+source("../../../src/analyzer/move_move_stack.R", chdir = TRUE)
 library(testthat)
 
-test_data <- readRDS(file = "./data/move_move_stack/input3_stork.rds")
+test_data <- readRDS(file = "../data/move_move_stack/input3_stork.rds")
 
-test_that("analyze non-empty-result", {
+test_that("non-empty-result", {
   actual <- analyzeMoveMoveStack(rds = test_data)
   expect_equal(actual$n[1], "non-empty-result")
+})
+
+test_that("null-result", {
+  actual <- analyzeMoveMoveStack(rds = readRDS(file = "../data/N0.rds"))
+  expect_equal(actual$n[1], "empty-result")
+})
+
+test_that("null-result", {
+  actual <- analyzeMoveMoveStack(rds = readRDS(file = "../data/N1.rds"))
+  expect_equal(actual$n[1], "one-item-result")
 })
