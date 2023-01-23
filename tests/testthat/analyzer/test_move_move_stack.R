b54ce603a1fe8ca3818b124d8c76be02cbc15f4f
annescharf <ascharf@ab.mpg.de>
2023-01-23 12:16:20 +0100

    correcting path names and uncommenting changes

--- tests/testthat/analyzer/test_move_move_stack.R
+++ tests/testthat/analyzer/test_move_move_stack.R
@@ -1,7 +1,7 @@
-source("src/analyzer/move_move_stack.R", chdir = TRUE)
+source("../../../src/analyzer/move_move_stack.R", chdir = TRUE)
 library(testthat)
 
-test_data <- readRDS(file = "tests/testthat/data/move_move_stack/input2_geese.rds")
+test_data <- readRDS(file = "../data/move_move_stack/input2_geese.rds")
 
 test_that("non-empty-result", {
   actual <- analyzeMoveMoveStack(rds = test_data)
@@ -28,31 +28,31 @@ test_that("timestamps", {
 
 test_that("animals", {
   actual <- analyzeMoveMoveStack(rds = test_data)
-  # iddata <- idData(test_data)
-  # names(iddata) <- make.names(names(iddata),allow_=FALSE)
-  # animalNames <- if(is.null(iddata$individual.local.identifier)){iddata$local.identifier}
-  
-  expect_equal(actual$animals_total_number[1], 3)
-  # expect_equal(actual$animals_total_number[1], n.indiv(test_data))
-  expect_equal(actual$animal_names[1], 742)
-  # expect_equal(actual$animal_names[1], animalNames[1])
+  iddata <- idData(test_data)
+  names(iddata) <- make.names(names(iddata),allow_=FALSE)
+  animalNames <- if(is.null(iddata$individual.local.identifier)){iddata$local.identifier}
+
+  # expect_equal(actual$animals_total_number[1], 3)
+  expect_equal(actual$animals_total_number[1], n.indiv(test_data))
+  # expect_equal(actual$animal_names[1], 742)
+  expect_equal(actual$animal_names[1], animalNames[1])
 })
 
 test_that("attribs", {
   actual <- analyzeMoveMoveStack(rds = test_data)
   expect_true(all(is.character(actual$animal_attributes)))
-  expect_equal(length(actual$animal_attributes),5)
-  # expect_equal(length(actual$animal_attributes),length(names(unique(test_data@idData)[,!sapply(test_data@idData, function(x) all(is.na(x)))])))
-  expect_equal(length(actual$track_attributes),16)
-  # expect_equal(length(actual$track_attributes),length(names(test_data@data[,!sapply(test_data@data, function(x) all(is.na(x)))])))
+  # expect_equal(length(actual$animal_attributes),5)
+  expect_equal(length(actual$animal_attributes),length(names(unique(test_data@idData)[,!sapply(test_data@idData, function(x) all(is.na(x)))])))
+  # expect_equal(length(actual$track_attributes),16)
+  expect_equal(length(actual$track_attributes),length(names(test_data@data[,!sapply(test_data@data, function(x) all(is.na(x)))])))
 })
 
 test_that("tracks", {
   actual <- analyzeMoveMoveStack(rds = test_data)
-  expect_equal(actual$tracks_total_number[1], 3)
-  # expect_equal(actual$tracks_total_number[1], length(namesIndiv(test_data))[1])
-  expect_equal(actual$track_names[1], "X742")
-  # expect_equal(actual$track_names[1], namesIndiv(test_data)[1])
+  # expect_equal(actual$tracks_total_number[1], 3)
+  expect_equal(actual$tracks_total_number[1], length(namesIndiv(test_data))[1])
+  # expect_equal(actual$track_names[1], "X742")
+  expect_equal(actual$track_names[1], namesIndiv(test_data)[1])
 })
 
 test_that("null-result", {
@@ -62,7 +62,7 @@ test_that("null-result", {
 #file N0.rds can never be received by an App, because it is not a moveStack, then it automatically becomes NULL
 
 test_that("one-item-result", {
-  actual <- analyzeMoveMoveStack(rds = readRDS(file = "tests/testthat/data/move_move_stack/N1.rds")) 
+  actual <- analyzeMoveMoveStack(rds = readRDS(file = "../data/move_move_stack/N1.rds")) 
   expect_equal(actual$n[1], "one-item-result") 
   expect_equal(actual$animals_total_number[1],1)
 })
