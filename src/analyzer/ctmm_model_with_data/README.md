42e747f3f56c1fcb2584f9416e2158f34d365bac
Johannes <jmsigner@gmail.com>
2023-05-15 15:29:12 +0200

    Added a new IO type: ctmm model with data. This is a list with two entries: 1) a list of fitted ctmm models and 2) a list of ctmm telemetry objects that were used to fit the models.

---
+++ src/analyzer/ctmm_model_with_data/README.md
@@ -0,0 +1,8 @@
+R CTMM Model with Data
+
+This IO type is to work with the R ctmm package, that requires a fitted ctmm model as input. This IO type consists of a list of length two. The first element of the list a list with fitted ctmm models and the second element is a list of ctmm data (see also the IO type ctmm telemetry list).
+
+Reference to ctmm package: https://ctmm-initiative.github.io/ctmm/reference/index.html
+
+Reference to details about ctmm model: https://ctmm-initiative.github.io/ctmm/reference/ctmm.fit.html
+Reference to details about telemetry object: https://ctmm-initiative.github.io/ctmm/reference/as.telemetry.html
---
+++ tests/testthat/test_ctmm_model_with_data.R
@@ -0,0 +1,39 @@
+library(testthat)
+
+io_type_slug = "ctmm_model_with_data"
+sut(io_type_slug)
+
+test.data <- test_data(io_type_slug, "buffalo.rds")
+test0 <- test_data(io_type_slug, "N1.rds")
+
+test_that("analyze one-item-result", {
+  actual <- analyzeCtmmModelWithData(rds = test.data)
+  expect_equal(actual$n[1], "non-empty-result")
+  expect_equal(actual$animals_total_number, 2)
+})
+
+test_that("proj", {
+  actual <- analyzeCtmmModelWithData(rds = test.data)
+  expect_true(all(substring(actual$projection, 1, 5) == "+proj"))
+})
+
+test_that("null-result",{
+  actual <- analyzeCtmmModelWithData(NULL)
+  expect_equal(actual$n[1],"empty-result")
+})
+
+
+test_that("one-result",{
+  actual <- analyzeCtmmModelWithData(rds = N1)
+  expect_equal(actual$n[1],"non-empty-result")
+})
+
+test_that("mix-with-empty-id",{
+  actual <- analyzeCtmmModelWithData(test)
+  expect_equal(actual$n[1],"non-empty-result")
+  expect_equal(actual$animals_total_number, 2) # this 2 is hardcoded, just note for the future in case there is a problem
+})
+
+
+
+
