53ebdf2e01caccf32a260cd027c295f991b12b24
andreakoelzsch <akoelzsch@ab.mpg.de>
2023-05-16 10:09:22 +0200

    Update test_move2_move2_nonloc.R

--- tests/testthat/analyzer/test_move2_move2_nonloc.R
+++ tests/testthat/analyzer/test_move2_move2_nonloc.R
@@ -1,23 +1,24 @@
-source("../../../src/analyzer/move2_move2_nonloc.R", chdir = TRUE)
-#source("src/analyzer/move2_move2_nonloc.R", chdir = TRUE)
-library(testthat)
+# system under test (SUT)
+io_type_slug = "move2_move2_nonloc"
+sut(io_type_slug)
 
-test_data <- readRDS(file = "../data/move2_move2_nonloc/example_nonloc_acc_burst_3geese.rds")
-#test_data <- readRDS(file = "tests/testthat/data/move2_move2_nonloc/example_lonloc_acc_burst_3geese.rds")
+# test data
+
+test_data <- test_data(io_type_slug, "example_nonloc_acc_burst_3geese.rds")
 
 test_that("non-empty-result", {
-  actual <- analyzeMove2Move2_nonloc(rds = test_data)
+  actual <- analyze(rds = test_data)
   expect_equal(actual$n[1], "non-empty-result")
   expect_equal(actual$animals_total_number,3)
 })
 
 test_that("timestamps", {
-  actual <- analyzeMove2Move2_nonloc(rds = test_data)
+  actual <- analyze(rds = test_data)
   expect_equal(length(actual$timestamps_range),2)
 })
 
 test_that("animals", {
-  actual <- analyzeMove2Move2_nonloc(rds = test_data)
+  actual <- analyze(rds = test_data)
   iddata <- mt_track_data(test_data)
   names(iddata) <- make.names(names(iddata),allow_=FALSE)
   if (!is.null(iddata$individual.local.identifier)) animalNames <- iddata$individual.local.identifier else animalNames <- iddata$local.identifier
@@ -29,7 +30,7 @@ test_that("animals", {
 })
 
 test_that("attribs", {
-  actual <- analyzeMove2Move2_nonloc(rds = test_data)
+  actual <- analyze(rds = test_data)
   expect_true(all(is.character(actual$animal_attributes)))
   # expect_equal(length(actual$animal_attributes),5)
   expect_equal(length(actual$animal_attributes),length(names(unique(mt_track_data(test_data))[,!sapply(mt_track_data(test_data), function(x) all(is.na(x)))])))
@@ -38,7 +39,7 @@ test_that("attribs", {
 })
 
 test_that("tracks", {
-  actual <- analyzeMove2Move2_nonloc(rds = test_data)
+  actual <- analyze(rds = test_data)
   # expect_equal(actual$tracks_total_number[1], 3)
   expect_equal(actual$tracks_total_number, length(as.character(unique(mt_track_id(test_data)))))
   # expect_equal(actual$track_names[1], "X742")
@@ -46,9 +47,7 @@ test_that("tracks", {
 })
 
 test_that("null-result", {
-  actual <- analyzeMove2Move2_nonloc(readRDS("../data/move2_move2_nonloc/example_nonloc_empty.rds"))
-  #actual <- analyzeMove2Move2_nonloc(readRDS("tests/testthat/data/move2_move2_nonloc/example_nonloc_empty.rds"))
+  actual <- analyze(test_data(io_type_slug, "example_nonloc_empty.rds"))
   expect_equal(actual$n[1], "empty-result")
   expect_equal(actual$animals_total_number,0)
 })
-
