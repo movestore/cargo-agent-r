21b19718d6fea9609bc9a3f4b8602a8cd95ed4eb
annescharf <ascharf@ab.mpg.de>
2025-02-13 14:19:13 +0100

    delete as not relevant

--- tests/testthat/data/move2_move2_loc/creating_test_data.R
+++
@@ -1,6 +0,0 @@
-library(move2)
-in4 <- readRDS("/home/ascharf/AllGitSYNC/MoveAppsGit/Template_R_Function_App/data/raw/input4_move2loc_LatLon.rds")
-in3 <- readRDS("/home/ascharf/AllGitSYNC/MoveAppsGit/Template_R_Function_App/data/raw/input3_move2loc_LatLon.rds")
-stk <- mt_stack(in3,in4)
-mt_track_id(stk) <- "individual_local_identifier"
-saveRDS(stk,"~/AllGitSYNC/MoveAppsGit/cargo-agent-r/tests/testthat/data/move2_move2_loc/test_data_move2_loc.rds")
